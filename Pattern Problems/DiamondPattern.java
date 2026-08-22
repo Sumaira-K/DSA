efd3efd38a6ef0d1d8ccb9bdc9308e61d2834b44
Sumaira-K <sumairakhan.reveuse@gmail.com>
2026-08-22 13:33:23 +0530

    Add Diamond Pattern

---
+++ Pattern Problems/DiamondPattern.java
@@ -0,0 +1,40 @@
+public class DiamondPattern 
+{
+    public static void main(String[] args) 
+    {
+        int n = 5;
+
+        // Print the upper half of the diamond
+        for (int i = 1; i <= n; i++) 
+        {
+            // Print leading spaces
+            for (int j = 1; j <= n - i; j++) 
+            {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 1; j <= 2 * i - 1; j++) 
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Print the lower half of the diamond
+        for (int i = n - 1; i >= 1; i--) 
+        {
+            // Print leading spaces
+            for (int j = 1; j <= n - i; j++) 
+            {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 1; j <= 2 * i - 1; j++) 
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
